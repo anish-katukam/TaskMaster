102ea6cd03d375b0e24795e1a32950fe5b222e53
Anish Katukam <anish.katukam@yahoo.com>
2019-04-09 23:21:43 -0700

    basic hello world, look, i just wanted to get this running, i know it's not complete, don't judge me
    code and whatnot

---
+++ sample/Controller.java
@@ -0,0 +1,13 @@
+package sample;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class Controller {
+    public Label helloWorld;
+
+    public void sayHelloWorld(ActionEvent actionEvent) {
+        helloWorld.setText("Hello World!");
+    }
+}
