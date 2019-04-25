984b0a88b77b9bbca5d2a4d6404915f2e009efcd
poojapatel45 <32950431+poojapatel45@users.noreply.github.com>
2019-04-25 03:46:16 -0700

    Update Event.java
    ill change priority to an int, was just trying something out

--- srcmain/Event.java
+++ srcmain/Event.java
@@ -3,11 +3,57 @@ package srcmain;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public abstract class Event {
-
+public class Event {
     private String description;
-    private int priority;
+    private String priority;
     private LocalDate dueDate;
     private int status;
     private LocalDateTime modifiedDate;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public LocalDateTime getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public void setModifiedDate(LocalDateTime modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public Event(String description, String priority, LocalDate dueDate, int status) {
+        this.setDescription(description);
+        this.setPriority(priority);
+        this.setDueDate(dueDate);
+        this.setStatus(status);
+    }
 }
