5dabf08a2f9a44b5a6b978455808614fc830c89c
Anish Katukam <anish.katukam@yahoo.com>
2019-04-25 12:59:38 -0700

    Fixed the problems with JavaFX not finding the proper files and crashing

--- srcmain/IO.java
+++ srcmain/IO.java
@@ -11,14 +11,14 @@ public class IO {
     public void save() throws IOException {
         FileOutputStream fileOutStream = new FileOutputStream("TaskMaster-List.txt");
         ObjectOutputStream ObjOutStream = new ObjectOutputStream(fileOutStream);
-        ObjOutStream.writeObject(activeTasks); // Verify this is where its stored
+        //ObjOutStream.writeObject(activeTasks); // Verify this is where its stored
         ObjOutStream.close();
     }
 
     public void load() throws IOException {
         FileInputStream fileInputStream = new FileInputStream("TaskMaster-List.txt");
         ObjectInputStream ObjectInputStream = new ObjectInputStream(fileInputStream);
-        ArrayList<Club> clubs = (ArrayList<Club>) ObjectInputStream.readObject();
+        //ArrayList<Club> clubs = (ArrayList<Club>) ObjectInputStream.readObject();
         ObjectInputStream.close();
     }
 }
