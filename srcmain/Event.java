0f45fd43f7bdc71053c26bf02b687d81d7ade54a
poojapatel45 <32950431+poojapatel45@users.noreply.github.com>
2019-04-25 14:18:15 -0700

    Event.java

--- srcmain/Event.java
+++ srcmain/Event.java
@@ -3,57 +3,11 @@ package srcmain;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public class Event {
+public abstract class Event {
+
     private String description;
-    private String priority;
+    private int priority;
     private LocalDate dueDate;
     private int status;
     private LocalDateTime modifiedDate;
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getPriority() {
-        return priority;
-    }
-
-    public LocalDate getDueDate() {
-        return dueDate;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public LocalDateTime getModifiedDate() {
-        return modifiedDate;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setPriority(String priority) {
-        this.priority = priority;
-    }
-
-    public void setDueDate(LocalDate dueDate) {
-        this.dueDate = dueDate;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public void setModifiedDate(LocalDateTime modifiedDate) {
-        this.modifiedDate = modifiedDate;
-    }
-
-    public Event(String description, String priority, LocalDate dueDate, int status) {
-        this.setDescription(description);
-        this.setPriority(priority);
-        this.setDueDate(dueDate);
-        this.setStatus(status);
-    }
 }
