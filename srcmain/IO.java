92c12d1f9809c142aa4700131cd2b932b5beaad3
Gabriel Do <gabriel.do@protonmail.com>
2019-04-25 11:47:31 -0700

    Added save and load file. Need to do print report

--- srcmain/IO.java
+++ srcmain/IO.java
@@ -1,15 +1,24 @@
 package srcmain;
+import java.io.*;
+import java.util.ArrayList;
 
 public class IO {
 
     public void printReport() {
-    }
-
-    public void save() {
 
     }
 
-    public void load() {
-        
+    public void save() throws IOException {
+        FileOutputStream fileOutStream = new FileOutputStream("TaskMaster-List.txt");
+        ObjectOutputStream ObjOutStream = new ObjectOutputStream(fileOutStream);
+        ObjOutStream.writeObject(activeTasks); // Verify this is where its stored
+        ObjOutStream.close();
+    }
+
+    public void load() throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("TaskMaster-List.txt");
+        ObjectInputStream ObjectInputStream = new ObjectInputStream(fileInputStream);
+        ArrayList<Club> clubs = (ArrayList<Club>) ObjectInputStream.readObject();
+        ObjectInputStream.close();
     }
 }
