838877f5f547df9199f275884781e14e7ce6e5dd
poojapatel45 <32950431+poojapatel45@users.noreply.github.com>
2019-04-25 15:23:22 -0700

    Update Task.java

--- srcmain/Task.java
+++ srcmain/Task.java
@@ -48,11 +48,11 @@ public class Task {
         this.status = status;
     }
 
-     public String toString() {
-        return "Description: " + this.getDescription() + " " +
-               "Priority: " + this.getPriority() + " " +
-               "Due: " + this.getDueDate() + " " +
-               "Status: " + this.getStatus();
+    public String toString() {
+        return "Description: " + this.getDescription() + "      " +
+                "Priority: " + this.getPriority() + "       " +
+                "Due: " + this.getDueDate() + "     " +
+                "Status: " + this.getStatus();
     }
 
     //TODO: Implement Changelog<ChangeTask>
